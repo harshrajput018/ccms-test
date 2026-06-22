067a7e2d6cad70ab94edb141606ef2b0460546f8
harshrajput018 <46439089+harshrajput018@users.noreply.github.com>
2026-06-22 12:47:44 +0530

    Changed MainActivity.Java for zooming in effect

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,17 @@
 package io.ionic.starter;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        getBridge().getWebView().getSettings().setBuiltInZoomControls(true);
+        getBridge().getWebView().getSettings().setDisplayZoomControls(false);
+        getBridge().getWebView().getSettings().setSupportZoom(true);
+    }
+}
